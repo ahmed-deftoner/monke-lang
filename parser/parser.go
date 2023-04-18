bf138726c2faf58292839b8e0f8acddaa73d907a
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2023-04-18 14:17:19 +0500

    feat: parser start

---
+++ parser/parser.go
@@ -0,0 +1,41 @@
+package parser
+
+import (
+	"github.com/ahmed-deftoner/monke-lang/ast"
+	"github.com/ahmed-deftoner/monke-lang/lexer"
+	"github.com/ahmed-deftoner/monke-lang/tokens"
+)
+
+type Parser struct {
+    l *lexer.Lexer
+    
+    currToken tokens.Token
+    peekToken tokens.Token
+}
+
+func New(l *lexer.Lexer) *Parser  {
+    p := &Parser{l: l}
+    p.NextToken()
+    p.NextToken()
+    return p
+}
+
+func (p *Parser) NextToken()  {
+    p.currToken = p.peekToken
+    p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+    program := &ast.Program{}
+    program.Statements = []ast.Statement{}
+
+    for p.currToken.Type != tokens.EOF {
+        stmt := p.ParseStatements()
+        if stmt != nil {
+            program.Statements = append(program.Statements, stmt)
+        }
+        p.NextToken()
+    }
+
+    return program
+}
